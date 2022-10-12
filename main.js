52abe7dac7af28c1ff7fc50e02fee4433de4f75c
ManNicx <113735662+ManNicx@users.noreply.github.com>
2022-10-12 10:37:39 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+function add() {
+    
+const node = document.getElementById("tables");
+const clone = node.cloneNode(true);
+document.body.appendChild(clone);
+}
+
+
+function remove() {
+    const list = document.getElementById("hollow");
+ list.removeChild(list.lastElementChild);
+  
+ if(hollow==="0") {
+     
+ }
+}
+
+
